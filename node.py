3cfaf71a32d0522fb2afc131e2ddcb37647c382d
AkiraFukuda <akira_fukuda@outlook.com>
2022-06-29 00:02:13 -0400

    temp

--- node.py
+++ node.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 import datetime
@@ -7,119 +8,126 @@ import pandas as pd
 import scipy.fftpack as fft
 
 app_tag = "App1"
-filename = "hdd/noise1024.bin"
+read_size = 1024
+interval = 15
+read_times = 10
+filename = "hdd/noise1_1024.bin"
 
 record_fn0 = "hdd/bw_record_0.csv"
 record_fn1 = "hdd/bw_record_1.csv"
+window_length = 86400
 
-def prediction_noise_wave(samples, hi_freq_ratio, interval):
-    df = pd.read_csv(record_fn0)
-    sample_size = len(df)
-
-    amp = fft.fft(samples)/sample_size
-    amp_complex_h = amp
-    amp_h = np.absolute(amp_complex_h)
-
-    freq = fft.fftfreq(amp.size, interval)
-    freq_h = freq
-
-    if amp_h[0] > 1e-10:
-        threshold = np.max(np.delete(amp_h, 0, axis=0)) * hi_freq_ratio
-        dc = amp_h[0]
-        start_index = 1
-    else:
-        threshold = np.max(amp_h) * hi_freq_ratio
-        dc = 0
-        start_index = 0
-    
-    selected_freq = []
-    selected_amp = []
-    selected_complex=[]
-    for i in range(start_index,len(amp_h)):
-        if amp_h[i] >= threshold:
-            selected_freq.append(freq_h[i])
-            selected_amp.append(amp_h[i])
-            selected_complex.append(amp_complex_h[i])
-
-    selected_phase = np.arctan2(np.array(selected_complex).imag,np.array(selected_complex).real)
-
-    for i in range(len(selected_phase)):
-        if np.fabs(selected_phase[i])<1e-10:
-            selected_phase[i]=0.0
-    
-    return dc, selected_amp, selected_freq, selected_phase   
+def noise_prediction():
+    df0 = pd.read_csv(record_fn0, header=None, names=['origin', 'date', 'time', 'bw'])
+    df1 = pd.read_csv(record_fn1, header=None, names=['origin', 'date', 'time', 'bw'])
+    print(df0.to_dict())
+    print(df1.to_dict())
 
 def bw_write(start, bw):
 
-    start_int = round(start)
+    now_date = int(round(start) / window_length)
+    start_time_int = round(start) % window_length
 
     # Pandas method
-    # df = pd.read_csv(record_filename)
+    # df = pd.read_csv(record_fn)
     # size = len(df)
-    # df.loc[size] = {'origin': app_tag, 'time': start_int, 'bw': bw}
+    # df.loc[size] = {'origin': app_tag, 'date': now_date, 'time': start_time_int, 'bw': bw}
     # df.to_csv(record_filename, index=False)
 
-    # Sliding window via file 0 and file 1
-    f0 = open(record_fn0, 'r')
-    f0_firstline = f0.readline()
-    print(f0_firstline)
+    # Sliding window via 2 files
+    f = open(record_fn0, 'r+')
+    if os.path.getsize(record_fn0) != 0:
+        f_firstline = f.readline().split(',')
+        f_date = int(f_firstline[1])
+        if now_date - f_date >= 2:
+            f.truncate(0)
+    f.close()
 
-    return
-    
+    f = open(record_fn1, 'r+')
+    if os.path.getsize(record_fn1) != 0:
+        f_firstline = f.readline().split(',')
+        f_date = int(f_firstline[1])
+        if now_date - f_date >= 2:
+            f.truncate(0)
+    f.close()
+
+    if now_date & 1 == 0:
+        f = open(record_fn0, 'a+')
+    else:
+        f = open(record_fn1, 'a+')
 
     # File I/O method
-    f = open(record_fn0, 'a+')
-    content = app_tag + ',' + str(start_int) + ',' + str(bw) + '\n'
+    # f = open(record_fn0, 'a+')
+    content = app_tag + ',' + str(now_date) + ',' + str(start_time_int) + ',' + str(bw) + '\n'
     f.write(content)
     f.close()
 
-
-def partial_read(size, interval, predict_result):
+def fully_read(size, interval):
     bandwidth = []
-    for i in range(20):
-        print
-
-
-def work(size, interval):
-    bandwidth = []
-    for i in range(20):
+    for i in range(read_times):
         print("%s s"%(i*interval))
 
         print("Start reading")
         start = time.time()
         f = open(filename, "rb")
+        f.read()
+        f.close()
+        end_io = time.time()
+        print("End reading")
+        io_time = end_io - start
+
+        end_ana = time.time()
+        ana_time = end_ana - start
+        print("Analysis time = ", ana_time)
+        if ana_time > interval:
+            print("Analysis time is larger than the interval!")
+            
+        bw = size / io_time
+        bandwidth.append(bw)
+        bw_write(start, bw)
+        print("Perceived bandwidth = %f MB/s" % bw)
+        time.sleep(interval - ana_time)
+
+
+def partial_read(size, interval, predict_result):
+    bandwidth = []
+    for i in range(read_times):
+        print("%s s"%(i*interval))
+
+        print("Start reading")
+        start = time.time()
+        f = open(filename, "rb")
+        
         f.close()
         end_io = time.time()
         print("End reading")
         io_time = end_io - start
         
+        end_ana = time.time()
+        ana_time = end_ana - start
+        print("Analysis time = ", ana_time)
+        if ana_time > interval:
+            print("Analysis time is larger than the interval!")
+            
         bw = size / io_time
         bandwidth.append(bw)
         bw_write(start, bw)
         print("Perceived bandwidth = %f MB/s" % bw)
-
-        end_ana = time.time()
-        ana_time = end_ana - start
-        print("Analysis time = ", ana_time)
-
-        if ana_time > interval:
-            print("Analysis time is larger than interval!\n")
         time.sleep(interval - ana_time)
 
+def work():
+    # fully_read(read_size, interval)
+    noise_prediction()
 
 def main():
-    # read_size = int(sys.argv[1])
-    # interval = int(sys.argv[2])
-    # if sys.argv[3] == 'now':
-    #     work(read_size, interval)
-    # else:
-    #     while True:
-    #         now_time = datetime.datetime.now(timezone('EST'))
-    #         if now_time.hour == int(sys.argv[3]) and now_time.minute == int(sys.argv[4]) and now_time.second == int(sys.argv[5]):
-    #             work(read_size, interval)
-    #             break
-
-    bw_write(100, 110)
+    if sys.argv[1] == 'now':
+        work()
+    else:
+        while True:
+            now_time = datetime.datetime.now(timezone('EST'))
+            if now_time.hour == int(sys.argv[1]) and now_time.minute == int(sys.argv[2]) and now_time.second == int(sys.argv[3]):
+                work()
+                break
 
 
 if __name__ == "__main__":
